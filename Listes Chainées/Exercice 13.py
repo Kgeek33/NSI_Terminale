4a3e668f57d8d49a4439b3228b92a1ef4dc6ffea
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2024-11-04 14:38:09 +0000

    occurences en version itérative et recursive

---
+++ Listes Chainées/Exercice 13.py
@@ -0,0 +1,57 @@
+class Maillon:
+    def __init__(self, v: any, s: any) -> None:
+        self._valeur = v
+        self._suivant = s
+
+    def __str__(self, chaine="|"):
+        "__str__() n'attend pas une liste vide"
+        if Maillon.est_vide(self._suivant):
+            return chaine+str(self._valeur)+"|"
+        else:
+            return chaine+"{}->{}".format(self.valeur(), Maillon.__str__(self.suite(), ""))
+
+    # retourne la valeur du maillon
+    def valeur(self) -> any:
+        return self._valeur
+
+    # retourne la queue de la liste
+    def suite(self) -> any:
+        return self._suivant
+
+    # methode de classe (partagée par toutes les instances)
+    def est_vide(lst) -> bool:
+        return lst == None
+
+
+def occurences_iter(x: any, lst: Maillon) -> int:
+    a = 0
+    while lst != None:
+        if lst.valeur() == x:
+            a += 1
+        lst = lst.suite()
+
+    return None if a == 0 else a
+
+def occurences_rec(x: any, lst: Maillon) -> int:
+    s = 0
+    if lst == None:
+        return 0
+    elif lst.valeur() == x:
+        s += 1
+
+    return occurences_rec(x, lst.suite()) + s
+
+m = Maillon(9, None)
+L1 = m
+
+m1 = Maillon(6, m)
+L2 = m1
+
+m2 = Maillon(3, m1)
+L3 = m2
+
+L4 = Maillon(30, Maillon(44, Maillon(30, None)))
+
+
+print(occurences_iter(30, L4))
+print(occurences_rec(30, L4))
