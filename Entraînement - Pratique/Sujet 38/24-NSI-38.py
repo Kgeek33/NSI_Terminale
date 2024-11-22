ebdd5b51b5ed680166e79f0bc2a34eeb75bc7e23
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2024-11-22 07:27:57 +0000

    oupi

--- Entraînement - Pratique/Sujet 38/24-NSI-38.py
+++ Entraînement - Pratique/Sujet 38/24-NSI-38.py
@@ -1,21 +1,24 @@
-def renverse(pile):
+def renverse(pile: list[int]):
     '''renvoie une pile contenant les mêmes éléments que pile,
     mais dans l'ordre inverse.
     Cette fonction détruit pile.'''
-    pile_inverse = ... 
+    pile_inverse = []
     while pile != []:
-        ... .append(...) 
-    return ... 
+        pile_inverse.append(pile.pop())
+    return pile_inverse
 
 
 def positifs(pile):
     '''renvoie une pile contenant les éléments positifs de pile,
     dans le même ordre. Cette fonction détruit pile.'''
-    pile_positifs = ... 
+    pile_positifs = []
     while pile != []:
-        ... = pile.pop() 
-        if ... >= 0: 
-            ...
-    return ... 
+        a = pile.pop()
+        if a >= 0:
+            pile_positifs.append(a)
+    return renverse(pile_positifs)
 
 
+assert renverse([1, 2, 3, 4, 5]) == [5, 4, 3, 2, 1]
+assert positifs([-1, 0, 5, -3, 4, -6, 10, 9, -8]) == [0, 5, 4, 10, 9]
+assert positifs([-2]) == []
