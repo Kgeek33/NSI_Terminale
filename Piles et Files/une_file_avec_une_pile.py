97e5d30bb3e1272fb7ce817769d933cc8e0acb7f
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2024-11-23 10:45:15 +0000

    Exercice 10 très chiant j'trouve

---
+++ Piles et Files/une_file_avec_une_pile.py
@@ -0,0 +1,47 @@
+from une_pile_avec_une_liste_chainée_TBC import *
+
+
+class File:
+    """structure de file"""
+
+    def __init__(self):
+        self.pile_ar = Pile()
+        self.pile_av = Pile()
+
+    def est_vide(self):
+        return self.pile_av.est_vide() and self.pile_ar.est_vide()
+
+    def retirer(self):
+        """ retire le maillon de tête """
+        if self.est_vide():
+            raise ValueError("Une des 2 piles est vide !!")
+
+        v = self.pile_ar.depiler()
+        self.pile_av.empiler(v)
+
+        return v
+
+    def ajouter(self, x):
+        self.pile_ar.empiler(x)
+
+    # permet l'appel str(L1) equivalent Ã  L1.__str__()
+    def __str__(self, chaine="["):
+        a = inverser_pile(self.pile_ar)
+        return f"Pile avant : {str(self.pile_av)} et Pile arrière {str(a)}"
+
+    def __len__(self):
+        return self.pile_ar.taille()
+
+
+F = File()
+print("file vide ?", F.est_vide())
+F.ajouter(5)
+print(str(F))
+F.ajouter(8)
+print(str(F))
+F.ajouter(-2)
+print(str(F))
+n = F.retirer()
+print("premier retiré =", n)
+print("file vide ?", F.est_vide())
+print(str(F))
