b181c2b0377dd86d0882cd5d65fc2ed7cec2dd46
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2024-11-23 10:50:32 +0000

    facile

--- Entraînement - Pratique/Sujet 37/24-NSI-37.py
+++ Entraînement - Pratique/Sujet 37/24-NSI-37.py
@@ -1,7 +1,8 @@
 def moyenne(tab: list) -> float:
     if len(tab) == 0:
-        raise ValueError("P.E.K.K.A UNE LISTE VIDE ?????????????????????????")
-    
+        raise ValueError(
+            "On ne peut pas faire une moyenne avec une liste vide !")
+
     moyenne = 0
     for valeur in tab:
         moyenne += valeur
@@ -9,22 +10,27 @@ def moyenne(tab: list) -> float:
 
     return moyenne
 
-assert moyenne([5,3,8]) == 5.333333333333333
-assert  moyenne([1,2,3,4,5,6,7,8,9,10]) == 5.5
+
+assert moyenne([5, 3, 8]) == 5.333333333333333
+assert moyenne([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5
 moyenne([])
+
 
 def tri(tab: list[0 | 1]):
     '''tab est un tableau d'entiers contenant des 0 et des 1.
     La fonction trie ce tableau en plaçant tous les 0 à gauche'''
-    i = ... # premier indice de la zone non triée 
-    j = ... # dernier indice de la zone non triée 
+    i = 0  # premier indice de la zone non triée
+    j = len(tab) - 1  # dernier indice de la zone non triée
     while i < j:
         if tab[i] == 0:
-            i = ... 
+            i = i + 1
         else:
-            valeur = ... 
-            tab[j] = ... 
-            ...
-            j = ... 
+            valeur = tab[j]
+            tab[j] = tab[i]
+            tab[i] = valeur
+            j = j - 1
 
 
+tab = [0, 1, 0, 1, 0, 1, 0, 1, 0]
+tri(tab)
+assert tab == [0, 0, 0, 0, 0, 1, 1, 1, 1]
