73cade3d5103f6819a1f80bd5a390b8c7b12a7f0
Lascar.pnj <labyrinthus3340@gmail.com>
2024-12-20 09:32:34 +0100

    maj

--- Bases de Données/Partie 4/Exercice 2.sql
+++ Bases de Données/Partie 4/Exercice 2.sql
@@ -1,2 +1,4 @@
-SELECT Nom_Station, Lieu, Region
-FROM Stations;
+SELECT Nom,Sejours.Station,Stations.Lieu,Stations.Region,Stations.Tarif
+FROM Sejours JOIN Clients ON Id_Client = Id
+JOIN Stations ON Sejours.Station = Nom_Station
+ORDER BY Stations.Tarif*Sejours.Nb_Places
