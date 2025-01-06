8c7acecbdac5c368854de0282c0a11190325f268
Raphaël <191569100+Skyniraph@users.noreply.github.com>
2025-01-06 14:42:42 +0100

    exo8 sql

---
+++ Bases de Données/TD BD Médiathèque/SQL/exo 8.sql
@@ -0,0 +1,4 @@
+select distinct A.isbn, e.titre, b.a_id, B.nom, B.prenom, C.a_id, C.nom, c.prenom
+from auteur_de A, auteur B,auteur C, auteur_de D, livre E
+where a.a_id = B.a_id and a.isbn = d.isbn and c.a_id !=a.a_id and c.a_id = d.a_id and a.isbn = e.isbn
+order by A.isbn, e.titre, b.a_id, B.nom, B.prenom, C.a_id, C.nom, c.prenom
