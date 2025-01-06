22937f6e79349371bc327b1cfd62042bc938925e
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-01-06 15:49:25 +0100

    update

---
+++ Bases de Données/SQL/Partie 6/Exercice 10.sql
@@ -0,0 +1,6 @@
+-- Question a --
+DELETE FROM Sejours WHERE Id_Client = 4;
+DELETE FROM Clients WHERE Id = 4;
+
+-- Question b --
+-- Voir question a --
--- Bases de Données/SQL/Partie 6/Exercice 8.sql
+++ Bases de Données/SQL/Partie 6/Exercice 8.sql
@@ -2,5 +2,8 @@
 INSERT INTO Clients
 VALUES(4,"Kariboo","Juliette","Toronto","Amérique",7213);
 
+INSERT INTO Sejours
+VALUES(4,"La Bourboule","2019-03-10",3);
+
 -- Question b --
 NULL;
