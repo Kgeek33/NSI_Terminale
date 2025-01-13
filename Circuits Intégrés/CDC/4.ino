87d436f6e106389a3def87ec7d069cd819d3bd87
Raphaël <191569100+Skyniraph@users.noreply.github.com>
2025-01-13 13:28:17 +0000

    ljkhgfdhjk

---
+++ Circuits Intégrés/CDC/4.ino
@@ -0,0 +1,23 @@
+const int BROCHE_LED_ROUGE = 2;
+const int BROCHE_POUSSOIR = 3;
+const int PERIOD = 500;
+bool fifi = false;
+
+void setup() {
+  pinMode(BROCHE_POUSSOIR, INPUT);
+  pinMode(BROCHE_LED_ROUGE, OUTPUT);
+}
+
+void loop() {
+  int bouton = digitalRead(BROCHE_POUSSOIR);
+  if (bouton == 1) {
+    fifi = !fifi;
+  }
+
+  if (fifi) {
+    digitalWrite(BROCHE_LED_ROUGE, HIGH) ;
+    delay(PERIOD);
+    digitalWrite(BROCHE_LED_ROUGE, LOW) ;
+    delay(PERIOD);
+  }
+}
