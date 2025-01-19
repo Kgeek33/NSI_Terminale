6746d1e60c805105a3fe73283936e483decf333d
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-01-19 18:31:29 +0000

    épreuve pratique N°39 complété !

--- Entraînement - Pratique/Sujet 39/24-NSI-39.py
+++ Entraînement - Pratique/Sujet 39/24-NSI-39.py
@@ -1,26 +1,49 @@
+def recherche(elt: int, tab: list[int]) -> int:
+    for i in range(len(tab) - 1, -1, -1):
+        if tab[i] == elt:
+            return i
+    return None
+
+
+assert recherche(1, [2, 3, 4]) == None
+assert recherche(1, [10, 12, 1, 56]) == 2
+assert recherche(1, [1, 0, 42, 7]) == 0
+assert recherche(1, [1, 50, 1]) == 2
+assert recherche(1, [8, 1, 10, 1, 7, 1, 8]) == 5
+
+
 class AdresseIP:
-    def __init__(self, adresse):
-        self.adresse =... 
+    def __init__(self, adresse: str):
+        self.adresse = adresse
 
     def liste_octets(self):
         """renvoie une liste de nombres entiers,
         la liste des octets de l'adresse IP"""
-        # Note : split découpe la chaine de caractères 
+        # Note : split découpe la chaine de caractères
         # en fonction du séparateur
         return [int(i) for i in self.adresse.split(".")]
 
     def est_reservee(self):
         """renvoie True si l'adresse IP est une adresse
         réservée, False sinon"""
-        reservees = [ ... ] 
-        return ... 
+        reservees = [0, 255]
+        return int(self.adresse.split(".")[3]) in reservees
 
     def adresse_suivante(self):
         """renvoie un objet de AdresseIP avec l'adresse
         IP qui suit l'adresse self si elle existe et None sinon"""
-        octets = ... 
-        if ... == 254: 
+        octets = self.liste_octets()[3]
+        if octets == 254:
             return None
-        octet_nouveau = ... + ... 
-        return AdresseIP('192.168.0.' + ...) 
+        octet_nouveau = octets + 1
+        return AdresseIP('192.168.0.' + str(octet_nouveau))
 
+
+adresse1 = AdresseIP("192.168.0.1")
+adresse2 = AdresseIP("192.168.0.2")
+adresse3 = AdresseIP("192.168.0.0")
+
+assert adresse1.liste_octets() == [192, 168, 0, 1]
+assert adresse1.est_reservee() == False
+assert adresse3.est_reservee() == True
+assert adresse2.adresse_suivante().adresse == "192.168.0.3"
