3f4f963c031a86f31d3e646c11af2f93930c64f2
Lascar.pnj <labyrinthus3340@gmail.com>
2025-01-23 14:17:15 +0000

    fix: lint

--- Arbres/classe_noeud.py
+++ Arbres/classe_noeud.py
@@ -44,6 +44,7 @@ def hauteur(self: Noeud):
         return 1 + hauteur(self.droit())
     return 1 + max(hauteur(self.droit()), hauteur(self.gauche()))
 
+
 def compte_feuilles(arbre: Noeud):
     if arbre == Noeud.arbre_vide:
         return 0
@@ -59,16 +60,16 @@ A_2 = Noeud("A", Noeud("B", None, None), Noeud("D", None, None))
 A_3 = Noeud("A", Noeud("B", None, Noeud("C", None, None)),
             Noeud("D", None, None))
 A_4 = Noeud(1,
-           Noeud(2,
-                 Noeud(4,
-                       Noeud(8, Noeud.arbre_vide, Noeud.arbre_vide),
-                       Noeud(9, Noeud.arbre_vide, Noeud.arbre_vide)),
-                 Noeud(5,
-                       Noeud(10, Noeud.arbre_vide, Noeud.arbre_vide),
-                       Noeud.arbre_vide)),
-           Noeud(3,
-                 Noeud(6, Noeud.arbre_vide, Noeud.arbre_vide),
-                 Noeud(7, Noeud.arbre_vide, Noeud.arbre_vide)))
+            Noeud(2,
+                  Noeud(4,
+                        Noeud(8, Noeud.arbre_vide, Noeud.arbre_vide),
+                        Noeud(9, Noeud.arbre_vide, Noeud.arbre_vide)),
+                  Noeud(5,
+                        Noeud(10, Noeud.arbre_vide, Noeud.arbre_vide),
+                        Noeud.arbre_vide)),
+            Noeud(3,
+                  Noeud(6, Noeud.arbre_vide, Noeud.arbre_vide),
+                  Noeud(7, Noeud.arbre_vide, Noeud.arbre_vide)))
 
 print(A_3.gauche().droit().valeur())
 print(A_3.est_feuille())
