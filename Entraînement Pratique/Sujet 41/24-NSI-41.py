3e560d209afd225d3cc356a6e2a157e776bc56fe
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-01-24 17:39:45 +0000

    exercice 1 du sujet 41

--- Entraînement Pratique/Sujet 41/24-NSI-41.py
+++ Entraînement Pratique/Sujet 41/24-NSI-41.py
@@ -5,6 +5,35 @@ class Noeud:
         self.droit = droit
 
 
+def taille(a: Noeud):
+    if a == None:
+        return 0
+
+    return 1 + taille(a.gauche) + taille(a.droit)
+
+
+def hauteur(a: Noeud):
+    if a is None:
+        return -1
+    if a.gauche is None and a.droit is None:
+        return 0
+    elif a.droit is None:
+        return 1 + hauteur(a.gauche)
+    elif a.gauche is None:
+        return 1 + hauteur(a.droit)
+    return 1 + max(hauteur(a.droit), hauteur(a.gauche))
+
+
+a = Noeud(1, Noeud(4, None, None), Noeud(0, None, Noeud(7, None, None)))
+
+assert hauteur(a) == 2
+assert taille(a) == 4
+assert hauteur(None) == -1
+assert taille(None) == 0
+assert hauteur(Noeud(1, None, None)) == 0
+assert taille(Noeud(1, None, None)) == 1
+
+
 def ajoute(indice, element, tab):
     '''Renvoie un nouveau tableau obtenu en insérant
     element à l'indice indice dans le tableau tab.'''
