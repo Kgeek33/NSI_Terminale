b7a286dd419d7845967e7ccbff97933b03de2458
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-01-24 17:32:15 +0000

    écriture de la class + indentation

--- Entraînement Pratique/Sujet 41/24-NSI-41.py
+++ Entraînement Pratique/Sujet 41/24-NSI-41.py
@@ -1,13 +1,18 @@
+class Noeud:
+    def __init__(self, etiquette, gauche, droit):
+        self.v = etiquette
+        self.gauche = gauche
+        self.droit = droit
+
+
 def ajoute(indice, element, tab):
     '''Renvoie un nouveau tableau obtenu en insérant
     element à l'indice indice dans le tableau tab.'''
     nbre_elts = len(tab)
     tab_ins = [0] * (nbre_elts + 1)
     for i in range(indice):
-        tab_ins[i] = ... 
-    tab_ins[...] = ... 
+        tab_ins[i] = ...
+    tab_ins[...] = ...
     for i in range(indice + 1, nbre_elts + 1):
-        tab_ins[i] = ... 
+        tab_ins[i] = ...
     return tab_ins
-
-
