3afcb9803aae23cc7c2c5984a47698c47f7f34d8
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-01-24 17:53:00 +0000

    exercice 1 du sujet 47

--- Entraînement Pratique/Sujet 47/24-NSI-47.py
+++ Entraînement Pratique/Sujet 47/24-NSI-47.py
@@ -1,5 +1,29 @@
+def max_dico(dico: dict[str, int]):
+    nom = None
+    valeur = None
+
+    for cle in dico:
+        if nom is None and valeur is None:
+            nom = cle
+            valeur = dico[cle]
+        else:
+            if dico[cle] > valeur:
+                nom = cle
+                valeur = dico[cle]
+    
+    return (nom, valeur)
+
+
+
+assert max_dico({'Bob': 102, 'Ada': 201, 'Alice': 103,
+                'Tim': 50}) == ('Ada', 201)
+assert max_dico({'Alan': 222, 'Ada': 201, 'Eve': 222, 'Tim': 50}) == (
+    'Alan', 222) or ('Eve', 222)
+
+
 class Pile:
     """Classe définissant une structure de pile."""
+
     def __init__(self):
         self.contenu = []
 
@@ -19,17 +43,16 @@ class Pile:
         assert not self.est_vide()
         return self.contenu.pop()
 
+
 def eval_expression(tab):
     p = Pile()
-    for ... in tab: 
-        if element != '+' ... element != '*': 
-            p.empiler(...) 
+    for ... in tab:
+        if element != '+' ... element != '*':
+            p.empiler(...)
         else:
-            if element == ...: 
-                resultat = ... + ... 
+            if element == ...:
+                resultat = ... + ...
             else:
-                resultat = ... 
-            p.empiler(...) 
-    return ... 
-
-
+                resultat = ...
+            p.empiler(...)
+    return ...
