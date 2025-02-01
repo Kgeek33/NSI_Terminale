015779d78e6dd88e0c44bf3e2c4ec847d18dd480
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-01 18:14:11 +0000

    exercice 2 du sujet 16

--- Entraînement Pratique/Sujet 16/24-NSI-16.py
+++ Entraînement Pratique/Sujet 16/24-NSI-16.py
@@ -13,16 +13,27 @@ assert ecriture_binaire_entier_positif(105) == "1101001"
 
 def echange(tab, i, j):
     '''Echange les éléments d'indice i et j dans le tableau tab.'''
-    temp = ...
-    tab[i] = ...
-    tab[j] = ...
+    temp = tab[i]
+    tab[i] = tab[j]
+    tab[j] = temp
 
 
 def tri_bulles(tab):
     '''Trie le tableau tab dans l'ordre croissant
     par la méthode du tri à bulles.'''
     n = len(tab)
-    for i in range(...):
-        for j in range(...):
-            if ... > ...:
-                echange(tab, j, ...)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if tab[j] > tab[j + 1]:
+                echange(tab, j, j + 1)
+
+
+tab = []
+tri_bulles(tab)
+assert tab == []
+tab2 = [9, 3, 7, 2, 3, 1, 6]
+tri_bulles(tab2)
+assert tab2 == [1, 2, 3, 3, 6, 7, 9]
+tab3 = [9, 7, 4, 3]
+tri_bulles(tab3)
+assert tab3 == [3, 4, 7, 9]
