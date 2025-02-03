c42ebcc1f79b062369ef1dbbedd2fd22d5f3c827
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-03 14:02:04 +0000

    fichier exo 1 complété

--- Arbres/TD_Villes_classe_ville.py
+++ Arbres/TD_Villes_classe_ville.py
@@ -9,11 +9,31 @@ class Ville:
 
     def __init__(self, liste: list[str]):
         self.nom = liste[0]
-        self.departement = int(liste[1])
+        self.departement = f"0{int(liste[1])}" if int(
+            liste[1]) < 10 else int(liste[1])
         self.population = int(liste[2])
         self.superficie = float(liste[3])
         self.rang = int(liste[4])
 
+    def __str__(self):
+        return (
+            f"{self.affiche_nom()} ({self.departement}),"
+            f"qui compte {self.population} "
+            f"habitants et couvre un territoire de {self.get_superficie()}km2"
+            ", est la "
+            f"{self.get_rang()}ème ville de France"
+        )
+
+    def get_rang(self):
+        return self.rang
+
+    def get_superficie(self):
+        return self.superficie
+
+    def affiche_nom(self):
+        return self.nom
+
 
 ligne_liste = ["Nice", "06", "343123", "71.9", "5"]
 ville = Ville(ligne_liste)
+print(ville)
