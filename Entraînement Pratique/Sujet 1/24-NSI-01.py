a5a5ef7b45061296de9b4bfaca63ac71f52d1957
Lascar.pnj <labyrinthus3340@gmail.com>
2025-02-08 18:37:54 +0000

    test

--- Entraînement Pratique/Sujet 1/24-NSI-01.py
+++ Entraînement Pratique/Sujet 1/24-NSI-01.py
@@ -1,18 +1,20 @@
 def echange(tab, i, j):
-    '''Echange les éléments d'indice i et j dans le tableau tab.'''
-    temp = ... 
-    tab[i] = ... 
-    tab[j] = ... 
+    '''Échange les éléments d'indice i et j dans le tableau tab.'''
+    temp = tab[i]
+    tab[i] = tab[j]
+    tab[j] = temp
 
 def tri_selection(tab):
     '''Trie le tableau tab dans l'ordre croissant
     par la méthode du tri par sélection.'''
     N = len(tab)
-    for k in range(...): 
-        imin = ... 
-        for i in range(..., N): 
-            if tab[i] < ...: 
-                imin = i
-        echange(tab, ..., ...) 
+    for k in range(N - 1):  
+        imin = k 
+        for i in range(k + 1, N):  
+            if tab[i] < tab[imin]:  
+                imin = i  
+        echange(tab, k, imin)  
 
 
+
+print(tri_selection([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]))  # [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
