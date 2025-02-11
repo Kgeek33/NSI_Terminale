da1a83498a2921ad62fa14498b12c06657136077
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-11 13:54:31 +0000

    fix(lint): in `exercice 8`

--- Récursicité/exercice 8.py
+++ Récursicité/exercice 8.py
@@ -1,12 +1,13 @@
 import math
 
+
 def nombre_de_chiffres(n):
     # Solution 1 : return len(str(n))
     # Solution 2 : return len(list(n))
     # Solution 3 :
     if n < 10:
         return 1
-    
+
     return nombre_de_chiffres(n // 10) + 1
 
 
