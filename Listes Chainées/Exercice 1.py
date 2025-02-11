600610f68a992eb536a37e604abc37750f09cb04
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-11 12:58:38 +0000

    fix(lin): in `Exercice 1`

--- Listes Chainées/Exercice 1.py
+++ Listes Chainées/Exercice 1.py
@@ -5,8 +5,10 @@ def concatener(t1: list, t2: list) -> list:
             t3.append(elm)
     return t3
 
+
 assert concatener([10, 20, 30], [100, 200, 300]) == [
     10, 20, 30, 100, 200, 300]
+
 
 def inserer(t: list, i: int, v: int) -> list:
     # t.insert(i, v)
@@ -15,5 +17,6 @@ def inserer(t: list, i: int, v: int) -> list:
         t[j] = t[j-1]
     t[i] = v
     return t
+
 
 assert inserer([10, 20, 30], 0, 3000) == [3000, 10, 20, 30]
