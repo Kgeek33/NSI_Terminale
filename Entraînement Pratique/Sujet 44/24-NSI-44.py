b858e46136abe31126ffa36f09f15a2cd56b1c08
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-16 20:17:15 +0000

    exercice 2 - sujet 44

--- Entraînement Pratique/Sujet 44/24-NSI-44.py
+++ Entraînement Pratique/Sujet 44/24-NSI-44.py
@@ -17,7 +17,7 @@ class Noeud:
     """Classe représentant un noeud d'un arbre binaire"""
 
     def __init__(self, etiquette, gauche, droit):
-        """Crée un noeud de valeur etiquette avec 
+        """Crée un noeud de valeur etiquette avec
         gauche et droit comme fils."""
         self.etiquette = etiquette
         self.gauche = gauche
@@ -27,22 +27,29 @@ class Noeud:
 def parcours(arbre, liste):
     """parcours récursivement l'arbre en ajoutant les étiquettes
     de ses noeuds à la liste passée en argument en ordre infixe."""
-    if arbre != None:
+    if arbre is not None:
         parcours(arbre.gauche, liste)
         liste.append(arbre.etiquette)
         parcours(arbre.droit, liste)
     return liste
 
 
-def insere(arbre, cle):
+def insere(arbre: Noeud, cle):
     """insere la cle dans l'arbre binaire de recherche
     représenté par arbre.
     Retourne l'arbre modifié."""
-    if arbre == None:
+    if arbre is None:
         return Noeud(cle, None, None)  # creation d'une feuille
     else:
-        if ...:
+        if arbre.gauche is not None:
             arbre.gauche = insere(arbre.gauche, cle)
         else:
-            arbre.droit = ...
+            arbre.droit = insere(arbre.droit, cle)
         return arbre
+
+
+arbre = Noeud(5, Noeud(2, None, Noeud(3, None, None)), Noeud(7, None, None))
+assert parcours(arbre, []) == [2, 3, 5, 7]
+for cle in (1, 4, 6, 8):
+    arbre = insere(arbre, cle)
+assert parcours(arbre, []) == [2, 3, 1, 4, 6, 8, 5, 7]
