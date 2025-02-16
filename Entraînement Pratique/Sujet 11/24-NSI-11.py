860a8188b827b6725e18f9a5bf63e20075d03d3f
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-16 18:54:59 +0000

    exercice 1 - sujet 11

--- Entraînement Pratique/Sujet 11/24-NSI-11.py
+++ Entraînement Pratique/Sujet 11/24-NSI-11.py
@@ -1,3 +1,28 @@
+def nombre_de_mots(phrase: str):
+    # Façon TBpM (rapide)
+    # if (phrase[-1] == "!" or phrase[-1] == "?"):
+    #     return len(phrase.split(" ")) - 1
+    # return len(phrase.split(" "))
+
+    # Autre façon (lente)
+    nbMots = 1
+    pList = list(phrase)
+    for elm in range(len(pList)):
+        if (
+            pList[elm] == " " and
+            pList[elm + 1] != "?" and
+            pList[elm + 1] != "!"
+        ):
+            nbMots += 1
+    return nbMots
+
+
+assert nombre_de_mots('Cet exercice est simple.') == 4
+assert nombre_de_mots('Le point d exclamation est séparé !') == 6
+assert nombre_de_mots('Combien de mots y a t il dans cette phrase ?') == 10
+assert nombre_de_mots('Fin.') == 1
+
+
 class Noeud:
     def __init__(self, etiquette):
         '''Méthode constructeur pour la classe Noeud.
@@ -13,11 +38,9 @@ class Noeud:
             if self.gauche != None:
                 ...
             else:
-                self.gauche = ... 
+                self.gauche = ...
         else:
             ...
-                ...
+            ...
             else:
-                ... = Noeud(cle) 
-
-
+                ... = Noeud(cle)
