cdae803319f0e01ad8489c86f9cd365fabe40b65
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-17 14:14:17 +0000

    fix(lint): in recherche_dichotomique

--- Diviser pour Régner/recherche_dichotomique tbc.py
+++ Diviser pour Régner/recherche_dichotomique tbc.py
@@ -1,32 +1,31 @@
-
-
-def recherche_dichotomique (t,v):
+def recherche_dichotomique(t, v):
     pass
 
 
-
-if __name__ == '__main__' :
-    def nextFibo(um2,um1):
-        return um1,um2+um1
+if __name__ == '__main__':
+    def nextFibo(um2, um1):
+        return um1, um2+um1
 
     def seqFibo(n):
-        umoins2=0;umoins1=1
-        F=[umoins2,umoins1]
-        for i in range(2,n):
-            umoins2,umoins1=nextFibo(umoins2,umoins1)
+        umoins2 = 0
+        umoins1 = 1
+        F = [umoins2, umoins1]
+        for i in range(2, n):
+            umoins2, umoins1 = nextFibo(umoins2, umoins1)
             F.append(umoins1)
         return F
 
-    T=seqFibo(16)
+    T = seqFibo(16)
     print(T)
-    print("55 est à la position",recherche_dichotomique(T,55))        # -> 10
-    print("56 est à la position",recherche_dichotomique(T,56))        # -> None
-    print("377 est à la position",recherche_dichotomique(T,377))      # -> 14
-    print("610 est à la position",recherche_dichotomique(T,610))      # -> 14
-    print("10000 est à la position",recherche_dichotomique(T,10000))  # -> None
-    print(recherche_dichotomique([0, 1, 1, 2, 3, 5, 8, 13, 21] , 7))
-    T=seqFibo(32)
+    print("55 est à la position", recherche_dichotomique(T, 55))  # -> 10
+    print("56 est à la position", recherche_dichotomique(T, 56))  # -> None
+    print("377 est à la position", recherche_dichotomique(T, 377))  # -> 14
+    print("610 est à la position", recherche_dichotomique(T, 610))  # -> 14
+    # -> None
+    print("10000 est à la position", recherche_dichotomique(T, 10000))
+    print(recherche_dichotomique([0, 1, 1, 2, 3, 5, 8, 13, 21], 7))
+    T = seqFibo(32)
     print(T)
-    print("55 est à la position",recherche_dichotomique(T,55))
-    print("196418 est à la position",recherche_dichotomique(T,196418)) # -> 27
-
+    print("55 est à la position", recherche_dichotomique(T, 55))
+    # -> 27
+    print("196418 est à la position", recherche_dichotomique(T, 196418))
