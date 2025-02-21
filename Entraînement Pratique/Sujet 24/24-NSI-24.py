ff3c216f75b4c5699c6d66a72c0e83dcb52aa19b
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-21 08:05:01 +0000

    exercice 1 - sujet 24

--- Entraînement Pratique/Sujet 24/24-NSI-24.py
+++ Entraînement Pratique/Sujet 24/24-NSI-24.py
@@ -1,19 +1,35 @@
+def parcours_largeur(arbre: tuple):
+    resultat = []
+    file = [arbre]
+    while file != []:
+        a = file.pop(0)
+        resultat.append(a[1])
+        if a[0] is not None:
+            file.append(a[0])
+        if a[2] is not None:
+            file.append(a[2])
+    return resultat
+
+
+arbre = (((None, 1, None), 2, (None, 3, None)),
+         4, ((None, 5, None), 6, (None, 7, None)))
+assert parcours_largeur(arbre) == [4, 2, 6, 1, 3, 5, 7]
+
+
 def somme_max(tab):
     n = len(tab)
     sommes_max = [0]*n
     sommes_max[0] = tab[0]
     # on calcule la plus grande somme se terminant en i
-    for i in range(1,n):
-        if ... + ... > ...: 
-            sommes_max[i] = ... 
+    for i in range(1, n):
+        if ... + ... > ...:
+            sommes_max[i] = ...
         else:
-            sommes_max[i] = ... 
+            sommes_max[i] = ...
     # on en déduit la plus grande somme de celles-ci
     maximum = 0
     for i in range(1, n):
-        if ... > ...: 
+        if ... > ...:
             maximum = i
 
-    return sommes_max[...] 
-
-
+    return sommes_max[...]
