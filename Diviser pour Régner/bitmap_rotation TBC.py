270b7c2bf49984329793be0453511a4d87abf76f
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-23 22:02:29 +0000

    fix(lint) errors

--- Diviser pour Régner/bitmap_rotation TBC.py
+++ Diviser pour Régner/bitmap_rotation TBC.py
@@ -1,9 +1,11 @@
 import PIL.Image as PIL
 img = PIL.open("riad-sattouf-librairies.png")
 largeur, hauteur = img.size        # dimensions de l'image en pixels
-# si 0<=x<largeur et 0<=y<hauteur, la couleur du pixel (x,y) est donnée par px[x,y]
+# si 0<=x<largeur et 0<=y<hauteur
+# la couleur du pixel (x,y) est donnée par px[x,y]
 px = img.load()
-# on peut modifier la couleur d'un pixel (x,y) avec l'affectation px[x,y]=c où c est une couleur
+# on peut modifier la couleur d'un pixel (x,y) avec
+# l'affectation px[x,y]=c où c est une couleur
 PIL.Image.show(img)
 
 
@@ -13,7 +15,8 @@ def rotation(px, x, y, t):
     t est une puissance de 2
     Ne renvoie rien """
     # rotation de chaque quart
-    # cas de base : un pixel tout seul : il tourne sur lui-même et rien ne change...
+    # cas de base : un pixel tout seul : il tourne sur lui-même
+    # et rien ne change...
     if t == 1:
         return None
 
