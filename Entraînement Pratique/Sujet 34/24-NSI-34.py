4903c770f66100815467af3744a097a402bce8b7
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-23 12:39:24 +0000

    exercice 1 - sujet 34 + indentation

--- Entraînement Pratique/Sujet 34/24-NSI-34.py
+++ Entraînement Pratique/Sujet 34/24-NSI-34.py
@@ -1,4 +1,19 @@
-def fusion(tab1,tab2):
+def nbr_occurrences(chaine: str) -> dict[str, int]:
+    UNdic = {}
+    chaineToList = list(chaine)
+    for UNcarac in chaineToList:
+        if UNcarac in UNdic:
+            UNdic[UNcarac] += 1
+        else:
+            UNdic[UNcarac] = 1
+    return UNdic
+
+
+assert nbr_occurrences("Hello world !") == {
+    'H': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 2, 'w': 1, 'r': 1, 'd': 1, '!': 1}
+
+
+def fusion(tab1, tab2):
     '''Fusionne deux tableaux triés et renvoie
     le nouveau tableau trié.'''
     n1 = len(tab1)
@@ -7,22 +22,20 @@ def fusion(tab1,tab2):
     i1 = 0
     i2 = 0
     i = 0
-    while i1 < n1 and ...: 
+    while i1 < n1 and ...:
         if tab1[i1] < tab2[i2]:
-            tab12[i] = ... 
-            i1 = ... 
+            tab12[i] = ...
+            i1 = ...
         else:
             tab12[i] = tab2[i2]
-            i2 = ... 
+            i2 = ...
         i += 1
     while i1 < n1:
-        tab12[i] = ... 
+        tab12[i] = ...
         i1 = i1 + 1
-        i = ... 
+        i = ...
     while i2 < n2:
-        tab12[i] = ... 
+        tab12[i] = ...
         i2 = i2 + 1
-        i = ... 
+        i = ...
     return tab12
-
-
