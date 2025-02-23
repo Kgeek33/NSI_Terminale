286518197e02a17681257ac789c3b5743193680f
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-02-23 22:01:53 +0000

    code complété

--- Diviser pour Régner/bitmap_rotation TBC.py
+++ Diviser pour Régner/bitmap_rotation TBC.py
@@ -1,13 +1,9 @@
-# Créé le 10/11/2020 en Python 3.4
-
 import PIL.Image as PIL
 img = PIL.open("riad-sattouf-librairies.png")
 largeur, hauteur = img.size        # dimensions de l'image en pixels
-# si 0<=x<largeur et 0<=y<hauteur, la couleur du pixel (x,y)
-# est donnée par px[x,y]
+# si 0<=x<largeur et 0<=y<hauteur, la couleur du pixel (x,y) est donnée par px[x,y]
 px = img.load()
-# on peut modifier la couleur d'un pixel (x,y) avec l'affectation px[x,y]=c
-# où c est une couleur
+# on peut modifier la couleur d'un pixel (x,y) avec l'affectation px[x,y]=c où c est une couleur
 PIL.Image.show(img)
 
 
@@ -17,13 +13,12 @@ def rotation(px, x, y, t):
     t est une puissance de 2
     Ne renvoie rien """
     # rotation de chaque quart
-    # cas de base : un pixel tout seul : il tourne sur lui-même et
-    # rien ne change...
-    # à completer
-    pass
+    # cas de base : un pixel tout seul : il tourne sur lui-même et rien ne change...
+    if t == 1:
+        return None
 
     # un carré non réduit à un point ...
-    # à completer
+    # rotation des 4 quarts
 
     rotation(px, x, y, t//2)
     rotation(px, x+t//2, y, t//2)
@@ -31,11 +26,14 @@ def rotation(px, x, y, t):
     rotation(px, x+t//2, y+t//2, t//2)
 
     # deplacement de chaque quart pîxel par pixel vers un autre quart
-    # à completer
     for xx in range(x, t//2):
         for yy in range(y, t//2):
             px_tmp = px[xx, yy+t//2]
             px[xx, yy+t//2] = px[xx, yy]
+            px[xx, yy] = px[xx+t//2, yy]
+            px[xx+t//2, yy] = px[xx+t//2, yy+t//2]
+            px[xx+t//2, yy+t//2] = px_tmp
+    return None
 
 
 rotation(px, 0, 0, largeur)
