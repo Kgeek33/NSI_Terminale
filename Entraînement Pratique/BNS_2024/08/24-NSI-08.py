e763f10bc8aa195fa1e32f751d971f86c765082d
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-03-06 13:50:09 +0000

    dossier décompressé : `BNS_2024.zip`

---
+++ Entraînement Pratique/BNS_2024/08/24-NSI-08.py
@@ -0,0 +1,70 @@
+#exo 1
+def delta(tab):
+    #precondition : tableau non vide
+    assert len(tab)!=0
+    #le tab à retourner contient en position 0 la première valeur du tableau a encoder
+    tab_comp=[tab[0]]
+    for k in range (1, len(tab)):
+        #on rajoute la différence de elemenst consécutifs de tab
+        tab_comp.append(tab[k]-tab[k-1])
+    return tab_comp
+    
+    
+assert delta([1000, 800, 802, 1000, 1003])==[1000, -200, 2, 198, 3]
+assert delta([42])==[42]
+
+
+#exo 2
+class Expr:
+    """Classe implémentant un arbre d'expression."""
+
+    def __init__(self, g, v, d):
+        """un objet Expr possède 3 attributs :
+        - gauche : la sous-expression gauche ;
+        - valeur : la valeur de l'étiquette, opérande ou nombre ;
+        - droite : la sous-expression droite."""
+        self.gauche = g
+        self.valeur = v
+        self.droite = d
+
+    def est_une_feuille(self):
+        """renvoie True si et seulement 
+        si le noeud est une feuille"""
+        return self.gauche is None and self.droite is None
+
+    def infixe(self):
+        """renvoie la représentation infixe de l'expression en
+        chaine de caractères"""
+        s = '' 
+        if self.gauche is not None:
+            s = '(' + s + self.gauche.infixe() 
+        s = s + str(self.valeur)
+        if self.droite is not None: 
+            s = s + self.droite.infixe() + ')'
+        return s
+
+a = Expr(Expr(None, 1, None),
+         '+',
+         Expr(None, 2, None))
+assert a.infixe()=='(1+2)'
+
+b = Expr(Expr(Expr(None, 1, None),
+              '+',
+              Expr(None, 2, None)),
+         '*',
+         Expr(Expr(None, 3, None),
+              '+',
+              Expr(None, 4, None)))
+assert b.infixe()=='((1+2)*(3+4))'
+e = Expr(Expr(Expr(None, 3, None),
+              '*',
+              Expr(
+                  Expr(None, 8, None),
+                  '+',
+                  Expr(None, 7, None))),
+             '-',
+             Expr(
+                 Expr(None, 2, None),
+                 '+',
+                 Expr(None, 1, None)))
+assert e.infixe()=='((3*(8+7))-(2+1))'
