c9ef5407d5113500d78346c0ad2b687fee0f81d7
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-03-10 14:41:12 +0000

    fix: lint errors

--- Les Graphes/graphe_matrice_adjacence_tbc.py
+++ Les Graphes/graphe_matrice_adjacence_tbc.py
@@ -2,15 +2,16 @@ class Graphe:
     """un graphe représenté par une matrice d'adjacence,
        où les sommets sont les entiers 0,1,...,n-1
        On ne traite pas les graphes pondérés ici :
-       la matrice est une matrice de booléens qui désignent l'existence ou non d'une arête"""
+       la matrice est une matrice de booléens qui désignent l'existence
+       ou non d'une arête"""
 
     def __init__(self, n):
-        """construit une matrice d'ordre n, représentant un graphe à n sommets sans arêtes"""
+        """construit une matrice d'ordre n, représentant un graphe
+        à n sommets sans arêtes"""
 
     def ajouter_arc(self, s1, s2):
         """prend en parametres les sommets s1 et s2 et
         modifie la matrice afin de créer un arc de s1 à s2"""
-   
 
     def arc(self, s1, s2):
         """prend en parametres les sommets s1 et s2 et
@@ -19,22 +20,20 @@ class Graphe:
     def ajouter_arete(self, s1, s2):
         """prend en parametres les sommets s1 et s2 et
         modifie la matrice afin de créer une arete de s1 à s2"""
-        
-
 
     def voisins(self, s):
         """prend en parametre un sommet s et
-        renvoie la liste de ses voisins (il existe une arc de s à chaque voisin)"""
+        renvoie la liste de ses voisins
+        (il existe une arc de s à chaque voisin)"""
 
     def affiche(self):
-        """affiche sur une ligne pour chaque sommet l'ensemble de ses voisins (dans un ordre indifférent)
+        """affiche sur une ligne pour chaque sommet l'ensemble de ses voisins
+        (dans un ordre indifférent)
         par ex :
         0 --> 1 3
         1 --> 2 3
         2 --> 3
         3 --> 1
         """
-        #à compléter...
+        # à compléter...
         pass
-
-
--- Les Graphes/une_file_avec_une_liste_chainée.py
+++ Les Graphes/une_file_avec_une_liste_chainée.py
@@ -1,7 +1,9 @@
-from  classe_maillon import *
+from classe_maillon import Maillon, taille_iter
+
 
 class File:
     """structure de file"""
+
     def __init__(self):
         self.tete = None
         self.queue = None
@@ -13,38 +15,39 @@ class File:
         if self.est_vide():
             raise IndexError("retirer sur une file vide")
         v = self.tete.valeur()
-        #disparition du maillon de tete
+        # disparition du maillon de tete
         self.tete = self.tete.suite()
-        #si la file est vide, il n'y a plus de queue
+        # si la file est vide, il n'y a plus de queue
         if self.tete is None:
             self.queue = None
         return v
 
     def ajouter(self, x):
         """ajoute un maillon en queue de la file"""
-        #creation du maillon de queue
+        # creation du maillon de queue
         m = Maillon(x, None)
         if self.est_vide():
             self.tete = m
         else:
-            #un peu moche !....
+            # un peu moche !....
             self.queue._suivant = m
         self.queue = m
 
-    def vider(self) :
-        self.tete=None
+    def vider(self):
+        self.tete = None
         self.queue = None
 
-    #permet l'appel str(L1) equivalent Ã  L1.__str__()
+    # permet l'appel str(L1) equivalent Ã  L1.__str__()
     def __str__(self, chaine="["):
         return str(self.tete)
 
     def __len__(self):
         return taille_iter(self.tete)
 
-if __name__ == '__main__' :
 
-    F= File()
+if __name__ == '__main__':
+
+    F = File()
     print("file vide ?", F.est_vide())
     F.ajouter(5)
     print(str(F))
@@ -52,10 +55,8 @@ if __name__ == '__main__' :
     print(str(F))
     F.ajouter(-2)
     print(str(F))
-    #n =premier(F)
-    n=F.retirer()
-    print("premier retiré =",n)
+    # n =premier(F)
+    n = F.retirer()
+    print("premier retiré =", n)
     print("file vide ?", F.est_vide())
     print(str(F))
-
-
