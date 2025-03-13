29c7c4a6d869807e65e4c60215a707220976cb4a
SaiKido33 <lucasdesaintdenis@gmail.com>
2025-03-13 13:37:20 +0000

    gzfscezg

--- Les Graphes/graphe_matrice_adjacence_tbc.py
+++ Les Graphes/graphe_matrice_adjacence_tbc.py
@@ -13,12 +13,12 @@ class Graphe:
     def ajouter_arc(self, s1, s2):
         """prend en parametres les sommets s1 et s2 et
         modifie la matrice afin de créer un arc de s1 à s2"""
-        self.adj[s1][s2] += 1
+        self.adj[s1][s2] = True
 
     def arc(self, s1, s2):
         """prend en parametres les sommets s1 et s2 et
         renvoie un booléen précisant l'existence ou non d'un arc de s1 à s2"""
-        return self.adj[s1][s2] > 0
+        return self.adj[s1][s2]
 
     def ajouter_arete(self, s1, s2):
         """prend en parametres les sommets s1 et s2 et
