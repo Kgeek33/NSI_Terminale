e0c578a2e4df6b667dc83dd6ea6105119e9e9bff
Lascar.pnj <labyrinthus3340@gmail.com>
2025-03-19 15:21:10 +0000

    commit 2 19/03/2025

--- Entraînement Pratique/Sujet 30/30/24-NSI-30.py
+++ Entraînement Pratique/Sujet 30/30/24-NSI-30.py
@@ -9,3 +9,27 @@ def traduire_romain(nombre):
         return ... 
 
 
+def fusion(tab1, tab2):
+    i, j = 0, 0
+    resultat = []
+    while i < len(tab1) and j < len(tab2):
+        if tab1[i] < tab2[j]:
+            resultat.append(tab1[i])
+            i += 1
+        else:
+            resultat.append(tab2[j])
+            j += 1
+    while i < len(tab1):
+        resultat.append(tab1[i])
+        i += 1
+    while j < len(tab2):
+        resultat.append(tab2[j])
+        j += 1
+    return resultat
+
+
+print(fusion([3, 5], [2, 5]))  # [2, 3, 5, 5]
+print(fusion([-2, 4], [-3, 5, 10]))  # [-3, -2, 4, 5, 10]
+print(fusion([4], [2, 6]))
+print(fusion([], []))  # []
+print(fusion([1, 2, 3], []))
