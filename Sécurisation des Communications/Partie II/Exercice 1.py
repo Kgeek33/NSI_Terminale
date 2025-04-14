bdc2c19d607c602c72298747bf39322a33699d3e
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-04-14 12:34:42 +0000

    Question 2

--- Sécurisation des Communications/Partie II/Exercice 1.py
+++ Sécurisation des Communications/Partie II/Exercice 1.py
@@ -1,1 +1,16 @@
 d = {chr(i+ord('A')): chr((i+13) % 26+ord('A')) for i in range(26)}
+
+
+def cesar_symetrique(s: str):
+    num = 0
+    chaine = ""
+    while num < len(s):
+        LAlettre = s[num]
+        chaine += d[LAlettre]
+        num += 1
+    return chaine
+
+
+if __name__ == "__main__":
+    assert cesar_symetrique("N") == "A"
+    assert cesar_symetrique("A") == "N"
