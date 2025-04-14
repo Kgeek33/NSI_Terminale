79ec8ce2d6478d7c27374906ba96b88ea610877a
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-04-14 19:58:21 +0000

    feat: regroupement de tout le code de Florent et moi et amélioration de la lisibilité du code

--- Les Graphes/TD Labyrinthe/labyrinthe.py
+++ Les Graphes/TD Labyrinthe/labyrinthe.py
@@ -3,56 +3,71 @@ from graphe_dictionnaire_adjacence_tbc import Graphe
 # from une_pile_avec_une_liste_chainée import Pile
 
 
-fen_princ = Tk()  # création d'une fenetre
-fen_princ.geometry("900x900")  # taille de la fenetre : 900x900
-monCanvas = Canvas(fen_princ, width=800, height=800, bg='green', border=10)
+# Fonctions d'initialisation
+def onkeypresse():
+    """ Cette fonction s'exécute lorsque la touche `r` est cliquée """
+    x = 100
+    y = 200
+    monCanvas.create_rectangle(x, y, x + 40, y + 40, fill="blue")
+
+
+def configGraphe(THEgraphe: Graphe):
+    """
+    Cette fonction configure les sommets et les arrêtes
+    à l'initialisation du graphe
+    """
+    # Ajout des sommets
+    for i in range(8):
+        for j in range(8):
+            THEgraphe.ajouter_sommet((i, j))
+
+    # Ajout des arrêtes des sommets
+    for i in range(8):
+        for j in range(8):
+            if (i, j) not in [(4, 2), (6, 6)]:
+                if i > 0 and (i - 1, j) not in [(4, 2), (6, 6)]:
+                    THEgraphe.ajouter_arrete((i, j), (i - 1, j))
+                if i < 7 and (i + 1, j) not in [(4, 2), (6, 6)]:
+                    THEgraphe.ajouter_arrete((i, j), (i + 1, j))
+                if j > 0 and (i, j - 1) not in [(4, 2), (6, 6)]:
+                    THEgraphe.ajouter_arrete((i, j), (i, j - 1))
+                if j < 7 and (i, j + 1) not in [(4, 2), (6, 6)]:
+                    THEgraphe.ajouter_arrete((i, j), (i, j + 1))
+
+
+# Initialisation de Tkinter
+fen_princ = Tk()
+fen_princ.geometry("900x900")
+fen_princ.bind("<KeyPress-r>", onkeypresse)
+
+# Initialisation du Canvas
+monCanvas = Canvas(fen_princ, width=800, height=800, bg="green", border=10)
+monCanvas.pack()
+
+# Initialisation d'un Graphe
 monGraphe = Graphe()
-for i in range(8):
-    for j in range(8):
-        monGraphe.ajouter_sommet((i, j))
-
-# widget canvas
-# il permet de dessiner des formes diverses
-monCanvas.pack()  # place le widget dans la fenetre
-
-# Ajout des arêtes entre les sommets, sauf pour (4, 2) et (6, 6)
-for i in range(8):
-    for j in range(8):
-        if (i, j) not in [(4, 2), (6, 6)]:  # Exclure les sommets (4, 2) et (6, 6)
-            if i > 0 and (i - 1, j) not in [(4, 2), (6, 6)]:
-                monGraphe.ajouter_arrete((i, j), (i - 1, j))
-            if i < 7 and (i + 1, j) not in [(4, 2), (6, 6)]:
-                monGraphe.ajouter_arrete((i, j), (i + 1, j))
-            if j > 0 and (i, j - 1) not in [(4, 2), (6, 6)]:
-                monGraphe.ajouter_arrete((i, j), (i, j - 1))
-            if j < 7 and (i, j + 1) not in [(4, 2), (6, 6)]:
-                monGraphe.ajouter_arrete((i, j), (i, j + 1))
+configGraphe(monGraphe)
+print("👇Voici le graphe 👇\n")
+monGraphe.affiche()
 
 
+# Amélioration de l'affichage de la fenêtre Tk
 def represente_laby(canevas: Canvas, G: Graphe):
+    """
+    Cette fonction configure l'écran pour représenter le Graphe
+    sous forme de rectangles avec une couleur spécifique
+    """
     for elm in G.sommets():
         x, y = elm
         x *= 100
         y *= 100
         if len(G.voisins(elm)) == 0:
-            canevas.create_rectangle(x, y, x + 40, y + 40, fill='blue')
+            canevas.create_rectangle(x, y, x + 40, y + 40, fill="blue")
         else:
-            canevas.create_rectangle(x, y, x + 40, y + 40, fill='white')
+            canevas.create_rectangle(x, y, x + 40, y + 40, fill="white")
 
 
-def onkeypresse(event):  # ...
-    x = 100
-    y = 200
-    monCanvas.create_rectangle(x, y, x + 40, y + 40, fill='blue')
-
-
-# permet d'appeler onkeypressed lorsque l'on appuie sur la touche <r>
-fen_princ.bind('<KeyPress-r>', onkeypresse)
-
-
-# création d'un rectangle de couleur bleue #de dimensions 40x40
-# lance le gestionnaire d'événements qui interceptera
-# les actions de l'utilisateur
-# monGraphe.affiche()
 represente_laby(monCanvas, monGraphe)
+
+# Lancement de l'intéraction avec l'écran
 fen_princ.mainloop()
