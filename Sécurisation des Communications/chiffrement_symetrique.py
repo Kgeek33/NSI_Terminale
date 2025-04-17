91111ede4cd31be7b78b03b858dfe4de1dcbf35c
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-04-17 12:12:22 +0000

    remplacement par le fichier du prof

---
+++ Sécurisation des Communications/chiffrement_symetrique.py
@@ -0,0 +1,71 @@
+d = {chr(i+ord('A')): chr((i+13)%26+ord('A')) for i in range(26)}
+# les espaces et les apostrohes ne sont cryptés
+d[' ']=' '
+d["'"]="'"
+print(d)
+
+
+# chiffrement cesar
+def chiffre_cesar(msg):
+    msg_chiffre=''
+    desaccents={'é':'e','è':'e','ê':'e','à':'a','ù':'u','ç':'c'}
+    for c_accent in desaccents:
+        msg= msg.replace(c_accent,desaccents[c_accent])
+    msg=msg.upper()
+    for c in msg:
+        msg_chiffre=msg_chiffre+d[c]   # concaténation avec le caractère crypté
+    return msg_chiffre
+
+print(chiffre_cesar("cyber cesar"))
+print(chiffre_cesar(chiffre_cesar("cyber cesar")))
+
+# chiffrement xor
+
+def unicode(s):
+    
+    return "a completer","a completer","a completer"
+
+message= "logiciel"
+u_dec,u_bin,u_hex=unicode(message)
+print("unicode (decimal) de '{}' : {}".format(message,u_dec))
+print("unicode (binaire) de '{}' : {}".format(message,u_bin))
+print("unicode (hexa) de '{}' : {}".format(message,u_hex))
+clef= "nsi"      
+u_dec,u_bin,u_hex=unicode(clef)
+print("unicode (decimal) de '{}' : {}".format(clef,u_dec))
+print("unicode (binaire) de '{}' : {}".format(clef,u_bin))
+print("unicode (hexa) de '{}' : {}".format(clef,u_hex))
+
+
+def chiffre_xor(msg,cle):
+    """prend les chaines msg et clé en paramètres et
+    renvoie la chaine des octets en hexa du message chiffré et
+    la liste de leur codage unicode (en decimal) """
+    "à compléter"
+    return "a completer","a completer"
+
+
+message="logiciel étoilé"
+u_dec,u_bin,u_hex=unicode(message)
+print("unicode (decimal) de '{}' : {}".format(message,u_dec))
+print("unicode (binaire) de '{}' : {}".format(message,u_bin))
+print("unicode (hexa) de '{}' : {}".format(message,u_hex))
+clef= "nsi"      
+u_dec,u_bin,u_hex=unicode(clef)
+print("unicode (decimal) de '{}' : {}".format(clef,u_dec))
+print("unicode (binaire) de '{}' : {}".format(clef,u_bin))
+print("unicode (hexa) de '{}' : {}".format(clef,u_hex))
+print("message chiffré : {}".format(chiffre_xor(message,clef)[0]))
+print("message chiffré (en decimal): {}".format(chiffre_xor(message,clef)[1]))
+
+
+
+
+def dechiffre_xor(msg_decimal,cle):
+    """prend la liste msg_decimal (liste des entiers codages unicode du msg chiffré)
+    et la clé en paramètres et renvoie la chaine du message déchiffré """
+    "à compléter"
+    return "à compléter"
+
+
+print("message déchiffré : {}".format(dechiffre_xor(chiffre_xor(message,clef)[1],clef),clef))
