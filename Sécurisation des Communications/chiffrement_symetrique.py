e41a37caa95236d18012aa154b0464d6ba54b4c4
SaiKido33 <lucasdesaintdenis@gmail.com>
2025-05-09 07:10:42 +0000

    piço_yiuyreyuretuir

--- Sécurisation des Communications/chiffrement_symetrique.py
+++ Sécurisation des Communications/chiffrement_symetrique.py
@@ -78,8 +78,14 @@ def dechiffre_xor(msg_decimal, cle):
     """prend la liste msg_decimal
     (liste des entiers codages unicode du msg chiffré)
     et la clé en paramètres et renvoie la chaine du message déchiffré """
-    "à compléter"
-    return "à compléter"
+    L = ""
+    for c in range(len(msg_decimal)):
+        if c >= len(cle) - 1:
+            L += chr(msg_decimal[c] ^ ord(cle[(c % len(cle))]))
+        else:
+            L += chr(msg_decimal[c] ^ ord(cle[c]))
+
+    return L
 
 
 print("message déchiffré : {}".format(
