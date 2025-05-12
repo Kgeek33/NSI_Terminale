1f37956dac0ea0e690b3aca32edf93a0a65c09a8
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-05-12 12:51:37 +0000

    ajout de tests

--- Sécurisation des Communications/chiffrement_asymetrique.py
+++ Sécurisation des Communications/chiffrement_asymetrique.py
@@ -3,3 +3,8 @@ def est_premier(n):
         if c % n == 0:
             return False
     return True
+
+
+if __name__ == "__main__":
+    for i in range(1, 5):
+        print(f"Numéro {i} premier ?? => ", est_premier(i))
