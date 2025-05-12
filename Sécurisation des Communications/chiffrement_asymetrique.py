cf72812f16511ed4770c658b28a2eba21cf9dec0
Lascar.pnj <labyrinthus3340@gmail.com>
2025-05-12 13:40:01 +0000

    idgfbskjdtrs

--- Sécurisation des Communications/chiffrement_asymetrique.py
+++ Sécurisation des Communications/chiffrement_asymetrique.py
@@ -28,10 +28,11 @@ def factorisation_RSA(n):
         return None
 
 
-
-
 if __name__ == "__main__":
     for i in range(1, 10):
         print(f"Numéro {i} premier ?? => ", est_premier(i))
     assert facteurs_premiers(300) == [2, 2, 3, 5, 5], "PAS BON"
     print(factorisation_RSA(377))
+    print(factorisation_RSA(437))
+    print(factorisation_RSA(99400891))
+    print(factorisation_RSA(99999640000243))
