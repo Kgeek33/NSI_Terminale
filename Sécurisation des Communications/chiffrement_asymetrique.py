c5d4c0f22a35054522ee9362b0a1a260d8e67083
SaiKido33 <lucasdesaintdenis@gmail.com>
2025-05-12 13:21:02 +0000

    rtjr'

--- Sécurisation des Communications/chiffrement_asymetrique.py
+++ Sécurisation des Communications/chiffrement_asymetrique.py
@@ -1,11 +1,26 @@
 from math import sqrt
 
+
 def est_premier(n):
-    for p in range(2,int(sqrt(n))+1):
+    for p in range(2, int(sqrt(n))+1):
         if n % p == 0:
             return False
     return True
 
+
+def facteurs_premiers(n):
+    i = 2
+    r = n
+    p = []
+    while r != 1:
+        if est_premier(i) and n % i == 0:
+            p.append(i)
+            r = n // i
+        i += 1
+    return p
+
+
+print(facteurs_premiers(300))
 
 if __name__ == "__main__":
     for i in range(1, 10):
