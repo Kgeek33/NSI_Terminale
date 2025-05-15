658a0008442d6c4dbf3e8222eaf4b017b1974209
Raphaël <191569100+Skyniraph@users.noreply.github.com>
2025-05-15 12:34:27 +0000

    zger

--- Securisation des Communications/chiffrement_asymetrique.py
+++ Securisation des Communications/chiffrement_asymetrique.py
@@ -40,10 +40,11 @@ def premiers_entre_eux(a: int, b: int) -> bool:
     return False
 
 
-def cles_valides(n: int, e: int , d: int) -> bool:
-    cle_publique = [e, n]
-    cle_privee = [d, n]
-    if :
+def cles_valides(n: int, e: int, d: int) -> bool:
+    p = factorisation_RSA(n)[0]
+    q = factorisation_RSA(n)[1]
+    f = (p-1) * (q-1)
+    if premiers_entre_eux(f, e) and p != q and p >= 3 and q >= 3 and e > 0 and e < f and est_premier(e) and d >= 1 and :
         return True
     return False
 
