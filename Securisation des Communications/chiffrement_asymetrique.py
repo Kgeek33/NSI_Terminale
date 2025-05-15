c4e8d90e49c1cbb4fa57698a6a13998d484ed5e3
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-05-15 13:02:06 +0000

    trop lent

--- Securisation des Communications/chiffrement_asymetrique.py
+++ Securisation des Communications/chiffrement_asymetrique.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     print(factorisation_RSA(377))
     print(factorisation_RSA(437))
     print(factorisation_RSA(99400891))
-    print(factorisation_RSA(99999640000243))
+    # print(factorisation_RSA(99999640000243))
 
     # Question 4
     print(premiers_entre_eux(2, 3))
