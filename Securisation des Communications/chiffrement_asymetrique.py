d308bc79987d82f8435647e9e402a161e6b7ee66
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-05-15 13:04:36 +0000

    ??????

--- Securisation des Communications/chiffrement_asymetrique.py
+++ Securisation des Communications/chiffrement_asymetrique.py
@@ -75,10 +75,10 @@ if __name__ == "__main__":
 
     # Question 5
     print(
-        "Clés (5, 377) et (269, 377) valides ? =>",
+        "Clés (5, 377) et (269, 377) valides ?? =>",
         cles_valides(377, 5, 269)
     )
     print(
-        "Clés (2, 377) et (269, 377) valides ? =>",
+        "Clés (2, 377) et (269, 377) valides ?? =>",
         cles_valides(377, 2, 269)
     )
