5e16f81d437e9880369b016aa42de326267e06bc
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-05-19 13:40:10 +0000

    ajout

--- Programmation Dynamique/algo_RenduMonnaie_glouton.py
+++ Programmation Dynamique/algo_RenduMonnaie_glouton.py
@@ -1,3 +1,6 @@
+euros = (2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01)
+
+
 def rendre_monnaie(pieces, s):
     q = 0
     P = list(pieces)
@@ -5,12 +8,13 @@ def rendre_monnaie(pieces, s):
     M = []
     a_rendre = s
     while q < len(P) and a_rendre > 0:
-        if a_rendre -P[q] > 0:
+        if a_rendre - P[q] > 0:
             a_rendre = round(a_rendre - P[q])
             M.append(P[q])
         else:
             q += 1
     return M
+
 
 if __name__ == '__main__':
     print(rendre_monnaie(euros, 5.33))
