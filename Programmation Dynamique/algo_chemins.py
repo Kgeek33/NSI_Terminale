5127e689eed5d3ff05931fe58855e78f52cf77f0
SaiKido33 <lucasdesaintdenis@gmail.com>
2025-05-23 07:37:27 +0000

    goiuyfuyifggjfx

---
+++ Programmation Dynamique/algo_chemins.py
@@ -0,0 +1,10 @@
+def chemin(n: int, m: int):
+    if n == 0 or m == 0:
+        return 1
+    return chemin(n-1, m) + chemin(n, m-1)
+
+
+if __name__ == '__main__':
+    assert chemin(0, 4) == 1
+    assert chemin(3, 4) == 35
+    assert chemin(10, 10) == 184756
