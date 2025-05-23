8bea6e745ba25dd658d87cbfd67cbfa6096f135d
Lascar.pnj <labyrinthus3340@gmail.com>
2025-05-23 06:37:14 +0000

    rtdfjghjytrthyt

--- Programmation Dynamique/fibo.py
+++ Programmation Dynamique/fibo.py
@@ -4,6 +4,22 @@ def fibo_rec(n: int) -> int:
     return fibo_rec(n-1) + fibo_rec(n-2)
 
 
+def fibo_memo(n, dico = {}):
+
+    if n == 0 or n == 1:
+        return 1
+    if n in dico:
+        return dico[n]
+    for i in range(n):
+        dico[n] = fibo_memo(n-1) + fibo_memo(n-2)
+    return dico[n]
+
+
+
+
+
 if __name__ == '__main__':
     for n in range(11):
         print(f"Fibonacci de {n} =>", fibo_rec(n))
+    for n in range(11):
+        print(f"Fibonacci mémo de {n} =>", fibo_memo(n))
