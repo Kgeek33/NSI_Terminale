071e894034a8765449ca88881653d2c7710271b4
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-05-26 13:34:51 +0000

    code

---
+++ Programmation Dynamique/pyramide.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+def generate_pyramide(h: int) -> list[list[int]]:
+    UNEliste: list[list[int]] = []
+    for i in range(1, h):
+        UNtableau = []
+        for _ in range(i):
+            UNtableau.append(randint(1, 9))
+        UNEliste.append(UNtableau)
+    return UNEliste
+
+
+if __name__ == "__main__":
+    print(generate_pyramide(5))
