189b49d262ae37082ff2749ce584e3b7317c9631
SaiKido33 <lucasdesaintdenis@gmail.com>
2025-05-26 12:57:20 +0000

    ttéf

--- Programmation Dynamique/algo_RenduMonnaie_optimal_dyn performance.py
+++ Programmation Dynamique/algo_RenduMonnaie_optimal_dyn performance.py
@@ -3,6 +3,7 @@ import functools
 from fibo import fibo_dyn, fibo_memo, fibo_rec
 from algo_RenduMonnaie_optimal_dyn import (
     nb_rendu, nb_rendu_dyn, nb_rendu_memo, euros,)
+from algo_chemins import chemin, chemins_memo, chemins_dyn
 
 
 def temps_d_execution(f, x):
@@ -20,9 +21,14 @@ def temps_d_execution_2(f, x, y):
 print(euros == (1, 2, 5, 20, 50, 100))
 a_rendre = 29
 n = 25
+i = 3
+j = 4
 # temps_d_execution_2(nb_rendu, euros, a_rendre)
 # temps_d_execution_2(nb_rendu_memo, euros, a_rendre)
 # temps_d_execution_2(nb_rendu_dyn, euros, a_rendre)
 temps_d_execution(fibo_rec, n)
 temps_d_execution(fibo_memo, n)
 temps_d_execution(fibo_dyn, n)
+temps_d_execution_2(chemin, i, j)
+temps_d_execution_2(chemins_memo, i, j)
+temps_d_execution_2(chemins_dyn, i, j)
