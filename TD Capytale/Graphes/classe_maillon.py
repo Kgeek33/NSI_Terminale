5eafd2cadcd48343545bf013db7a60e192f3483f
Raphaël <191569100+Skyniraph@users.noreply.github.com>
2025-05-26 13:20:36 +0000

    rtgezrergr

--- TD Capytale/Graphes/classe_maillon.py
+++ TD Capytale/Graphes/classe_maillon.py
@@ -139,7 +139,7 @@ if __name__ == '__main__':
     print(L1.valeur())  # 3
     print(Maillon.est_vide(L1))  # False
     print("lg (iter) de ", L1, ":", taille_iter(L1))
-    print("lg (recursif) de ",str(L1),taille_rec(L1))
+    print("lg (recursif) de ", str(L1), taille_rec(L1))
     c1 = Maillon(9, None)
     c2 = Maillon(6, c1)
     L2 = Maillon(3, c2)
