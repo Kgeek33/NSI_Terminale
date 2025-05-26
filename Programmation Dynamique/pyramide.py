09dac8eb6f9111b84da5c46a4f9151f315ceb9cf
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2025-05-26 13:37:39 +0000

    fix

--- Programmation Dynamique/pyramide.py
+++ Programmation Dynamique/pyramide.py
@@ -3,7 +3,7 @@ from random import randint
 
 def generate_pyramide(h: int) -> list[list[int]]:
     UNEliste: list[list[int]] = []
-    for i in range(1, h):
+    for i in range(1, h + 1):
         UNtableau = []
         for _ in range(i):
             UNtableau.append(randint(1, 9))
